e3a6dbb36281d5f2eebfbb915ee70293e49e84a6
primello <070194mp>
2019-06-25 17:30:50 -0300

    autoinfracao

--- src/main/java/br/gov/sc/ciasc/transito/ContagemPontos/services/AutoInfracaoService.java
+++ src/main/java/br/gov/sc/ciasc/transito/ContagemPontos/services/AutoInfracaoService.java
@@ -1,10 +1,31 @@
 package br.gov.sc.ciasc.transito.ContagemPontos.services;
 
+import br.gov.sc.ciasc.transito.ContagemPontos.domain.AutoInfracao;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AutoInfracaoService {
-    public int getContagemPontos(){
-    return 0;
+
+
+    public int somarPontos(List<AutoInfracao>infracaoList) {
+        int pontuacao = 0;
+
+        for (AutoInfracao autoInfracao : infracaoList) {
+            pontuacao += autoInfracao.getNumeroDePontos();
+
+
+        }
+
+        return pontuacao;
+
+
     }
+    public int somarPontosAnual(List<AutoInfracao> infracaoList){
+        return 0;
+    }
+
+
 }
+
--- src/test/java/br/gov/sc/ciasc/transito/ContagemPontos/domain/AutoInfracaoTest.java
+++ src/test/java/br/gov/sc/ciasc/transito/ContagemPontos/domain/AutoInfracaoTest.java
@@ -1,9 +1,15 @@
 package br.gov.sc.ciasc.transito.ContagemPontos.domain;
 
+import br.gov.sc.ciasc.transito.ContagemPontos.services.AutoInfracaoService;
+import jdk.vm.ci.meta.Local;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -34,8 +40,49 @@ public class AutoInfracaoTest {
         assertFalse(false);
     }
     @Test
-    public void VerificarContagemPontos(){
+    public void SomarPontos(){
+        AutoInfracao ai = new AutoInfracao();
+        ai.setPontos("4-Média");
+
         AutoInfracao ai1 = new AutoInfracao();
+        ai1.setPontos("5-Grave");
+
+       List<AutoInfracao> infracaolist = new ArrayList<>();
+       infracaolist.add(ai);
+       infracaolist.add(ai1);
+
+       AutoInfracaoService ais = new AutoInfracaoService();
+        //int contagemPontos = ais.somarPontos(infracaolist);
+        assertEquals(9,ais.somarPontos(infracaolist));
+    }
+    //@Ignore
+    @Test
+    public void SomarPontosAnual(){
+        AutoInfracao ai = new AutoInfracao();
+        ai.setPontos("3-Leve");
+        ai.setDataAutuacao(LocalDate.of(2018,7,24));
+
+        AutoInfracao ai2 = new AutoInfracao();
+        ai2.setPontos("4-Média");
+        ai2.setDataAutuacao(LocalDate.of(2018,12,25));
+
+        AutoInfracao ai3 =  new AutoInfracao();
+        ai3.setPontos("7-Gravíssima");
+        ai3.setDataAutuacao(LocalDate.of(2019,5,30));
+
+        AutoInfracao ai4 = new AutoInfracao();
+        ai4.setPontos("5-Leve");
+        ai4.setDataAutuacao(LocalDate.of(2019,9,1));
+
+        List<AutoInfracao> infracaoList = new ArrayList<>();
+        infracaoList.add(ai);
+        infracaoList.add(ai2);
+        infracaoList.add(ai3);
+        infracaoList.add(ai4);
+
+        AutoInfracaoService ais = new AutoInfracaoService();
+        assertEquals(14, ais.somarPontosAnual(infracaoList));
+
     }
 
 }
